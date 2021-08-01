a9e1bad3ca2b173b49b44abb6150ec1999e9a3ab
Harsh <rathiharsh2002@gmail.com>
2021-08-01 18:39:46 +0530

    Future Builder

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -6,32 +6,35 @@ import 'package:provider/provider.dart';
 import '../providers/cart.dart';
 import '../providers/orders.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget { 
   //convert to stateful to use didChangeDependencies etc
   // const OrdersScreen({ Key? key }) : super(key: key);
 
   static const routeName = '/orders-screen';
 
+  @override
+  _OrdersScreenState createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
   var _isLoading = false;
-  // var _isInit=true;
 
-  // @override
-  // void initState() {
-  //  _isLoading=true;
-  //   Provider.of<Orders>(context, listen: false)
-  //       .fetchAndSetOrders()
-  //       .then((value) {
-  //     setState(() {
-  //       _isLoading = false;
-  //     });
-  //   });
+  Future _ordersFuture () {
+    return Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+  }
 
-  //   super.initState();
-  // }
+  Future storeFutureOrders;
+
+  void initState(){
+  storeFutureOrders= _ordersFuture();
+  super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // final orderData = Provider.of<Orders>(context);
+    // final orderData = Provider.of<Orders>(context); //we would run into an 
+    // infinite loop as we notify the listeners here =>as build would keep building 
+    // =>solution =>use a consumer 
     print('build orders');
 
     return Scaffold(
@@ -39,14 +42,18 @@ class OrdersScreen extends StatelessWidget {
           title: Text('Orders Screen'),
         ),
         drawer: AppDrawer(),
-        body: FutureBuilder(
-          future:
-              Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
-          builder: (ctx, dataSnapshot) {
+        //as we can do this witout using stateful 
+        body: FutureBuilder( //starts listening to that
+        //builder gets the current snapshot of the future =>so that we can build different content based on the future
+          future:storeFutureOrders, //by this we ensure that no more future is created as it is a stateful widget 
+          //and the rest if the state can build anyways it wants 
+              
+          builder: (ctx, dataSnapshot) { //dataSnapshot which is returned by the future and the
+          // widget that we want to build depends on the futures data 
             if (dataSnapshot.connectionState == ConnectionState.waiting) {
               return Center(child: CircularProgressIndicator());
             } else {
-              if (dataSnapshot.error == null) {
+              if (dataSnapshot.error == null) { //np error 
                 return Consumer<Orders>(
                   builder: (ctx, orderData, child) {
                     return ListView.builder(
@@ -58,6 +65,7 @@ class OrdersScreen extends StatelessWidget {
                 );
               } else {
                 return Center(child: Text(dataSnapshot.error.toString()));
+                //..do error handling stuff 
               }
             }
           },
