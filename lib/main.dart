8e9949ca3ba37e1e9ee1ceee5ac1bc2986a31e46
Harsh <rathiharsh2002@gmail.com>
2021-08-21 22:27:17 +0530

    complete app

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/providers/auth.dart';
 import 'package:flutter_complete_guide/providers/product.dart';
 import 'package:flutter_complete_guide/screens/auth_screen.dart';
+import 'package:flutter_complete_guide/screens/splash_screen.dart';
 import 'package:provider/provider.dart';
 import './screens/edit_product_screen.dart';
 import 'screens/manage_products_screen.dart';
@@ -32,13 +33,15 @@ class MyApp extends StatelessWidget {
             //cleans up the data once used=>eg screen replaced
             //at this point we have access to the previously provided objets
             //whenever auth changes this provider rebuilds
-            create: (ctx) => Products('token', [],'user id'),
+            create: (ctx) => Products('token', [], 'user id'),
             // Provider.of<Auth>(context, listen: false).tokenData,
             // Provider.of<Products>(context, listen: false).items),
 
             //when we use the update method then the previous state gets erased
-            update: (ctx, auth, previousProducts) => Products(auth.tokenData,
-                previousProducts == null ? [] : previousProducts.items,auth.userId),
+            update: (ctx, auth, previousProducts) => Products(
+                auth.tokenData,
+                previousProducts == null ? [] : previousProducts.items,
+                auth.userId),
 
             //previousProducts->previous state of the products
             //we also need to initialize the private properties of the class to avoid to losse them
@@ -51,9 +54,11 @@ class MyApp extends StatelessWidget {
             create: (ctx) => Cart(),
           ),
           ChangeNotifierProxyProvider<Auth, Orders>(
-            create: (ctx) => Orders('token', [],'user id'),
-            update: (ctx, auth, previousOrders) => Orders(auth.tokenData,
-                previousOrders == null ? [] : previousOrders.orders,auth.userId),
+            create: (ctx) => Orders('token', [], 'user id'),
+            update: (ctx, auth, previousOrders) => Orders(
+                auth.tokenData,
+                previousOrders == null ? [] : previousOrders.orders,
+                auth.userId),
           ),
         ],
         child: Consumer<Auth>(
@@ -68,7 +73,15 @@ class MyApp extends StatelessWidget {
                   fontFamily: 'Lato'),
               home: auth.isAuth
                   ? ProductsOverviewScreen()
-                  : AuthScreen(), //after login/signup we want to change the home sceen =>so build should run again .
+                  : FutureBuilder(
+                      future: auth.tryAutoLogin(),
+                      builder: (c, authResultSnapshot) =>
+                          authResultSnapshot.connectionState ==
+                                  ConnectionState.waiting
+                              ? SplashScreen()
+                              : AuthScreen()),
+
+              //after login/signup we want to change the home sceen =>so build should run again .
               //authenticatd ->yes =>show ProductsOverview screen
               routes: {
                 // AuthScreen.routeName:(ctx)=>AuthScreen(),
--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -2,12 +2,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_complete_guide/models/http_exceptions.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:async';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Auth with ChangeNotifier {
   //provider in main.dart
   String _token; //expires at some point of time (eg. 1 hour)
   DateTime _expiryDate;
   String _userId;
+  Timer _authTimer;
 
   bool get isAuth {
     if (tokenData != null) {
@@ -19,8 +22,8 @@ class Auth with ChangeNotifier {
   String get tokenData {
     if (_expiryDate != null &&
         _expiryDate.isAfter(DateTime.now()) &&
-        _token !=null) 
-        //_expiryDate.isAfter(DateTime.now())->means expiry date sometime in the future
+        _token != null)
+    //_expiryDate.isAfter(DateTime.now())->means expiry date sometime in the future
     {
       return _token;
     }
@@ -54,8 +57,8 @@ class Auth with ChangeNotifier {
 // localId	    string	The uid of the newly created user.
 
       final responseData = json.decode(response.body);
-      if (responseData['error'] != null) { 
-        //sample error returned from firebase  
+      if (responseData['error'] != null) {
+        //sample error returned from firebase
         //{error: {code: 400, message: EMAIL_EXISTS, errors: [{message: EMAIL_EXISTS, domain: global, reason: invalid}]}}
         throw HttpExceptions(responseData['error']['message']);
       }
@@ -63,18 +66,50 @@ class Auth with ChangeNotifier {
       _userId = responseData['localId'];
       _expiryDate = DateTime.now()
           .add(Duration(seconds: int.parse(responseData['expiresIn'])));
+      autoLogout();
+
       notifyListeners();
-       print('sign in / login success');
+      print('sign in / login success');
+
+      //shared pref here
+      final prefs = await SharedPreferences.getInstance();
+      final userData = json.encode({
+        'token': _token,
+        'userId': _userId,
+        'expiryDate': _expiryDate.toIso8601String()
+      });
+      prefs.setString('userData',
+          userData); //Saves a string [value] to persistent storage in the background
+      
 
     } catch (error) {
-       print('error in sign up method');
+      print('error in sign up method');
       throw error;
-     
     }
   }
+//to retrieve the data set on the in device storage by shared prefs 
+  Future<bool> tryAutoLogin() async {
+    final prefs = await SharedPreferences.getInstance();
+    if(!prefs.containsKey('userData')) {
+      return false ;
+    }
+    final extractedUserData = json.decode(prefs.getString('userData')) as Map<String,Object>;
+    final expiryDate = DateTime.parse(extractedUserData['expiryDate']);
+
+    if(expiryDate.isBefore(DateTime.now())) {
+      return false;
+    }
+    _token =extractedUserData['token'];
+    _userId=extractedUserData['userId'];
+    _expiryDate=expiryDate;
+    notifyListeners();
+    autoLogout();
+    return true;
+  } 
+
 
   Future<void> signup(String email, String password) async {
-  //     https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
+    //     https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
     return _authenticate(email, password,
         'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCskLDea1LV3lGR20monGKGz7gUGWlkuT8');
   }
@@ -84,10 +119,34 @@ class Auth with ChangeNotifier {
         'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCskLDea1LV3lGR20monGKGz7gUGWlkuT8');
   }
 
-  Future<void> logout() {
-    _token =null;
-    _userId=null;
-    _expiryDate=null;
+  Future<void> logout() async{
+    _token = null;
+    _userId = null;
+    _expiryDate = null;
+    //whenever all these properties are set to null the bool
+    //isAuth builds again and we go back to the login page
+    if (_authTimer != null) {
+      _authTimer
+          .cancel(); //if we logout before the timer ends when we press the logout button
+          _authTimer=null;
+    }
+
     notifyListeners();
+    final prefs =await SharedPreferences.getInstance();
+    prefs.clear();
   }
+
+  void autoLogout() {
+    if (_authTimer != null) {
+      //cancel the pre esisting timer before setting the new timer
+      _authTimer.cancel();
+    }
+    final timeToExpire = _expiryDate.difference(DateTime.now()).inSeconds;
+    _authTimer = Timer(Duration(seconds: timeToExpire), () {
+      //when the timer is done
+      logout();
+    }); //A count-down timer that can be configured to fire once or repeatedly.
+  }
+
+  //for the auto login we need to save info on the app
 }
